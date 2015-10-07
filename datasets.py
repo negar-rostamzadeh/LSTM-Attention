222c453acffa43f38c334ca70c0fb648ad29d8a8
Mohammad Pezeshki <mohammadpz@gmail.com>
2015-10-07 11:50:44 -0400

    mnist video added

--- datasets.py
+++ datasets.py
@@ -2,12 +2,14 @@ import numpy
 import theano
 from fuel.streams import DataStream
 from fuel.transformers import Flatten
+from fuel.datasets import H5PYDataset
+from fuel.schemes import ShuffledScheme
+from fuel.transformers import Transformer
 floatX = theano.config.floatX
 
 
 def get_mnist_streams(num_train_examples, batch_size):
     from fuel.datasets import MNIST
-    from fuel.schemes import ShuffledScheme
     dataset = MNIST(("train",))
     all_ind = numpy.arange(dataset.num_examples)
     rng = numpy.random.RandomState(seed=1)
@@ -49,3 +51,52 @@ def get_memory_streams(num_train_examples, batch_size, time_length=15, dim=2):
     valid_stream = DataStream(dataset)
 
     return tarin_stream, valid_stream
+
+
+class PreprocessTransformer(Transformer):
+    def __init__(self, data_stream, **kwargs):
+        super(PreprocessTransformer, self).__init__(
+            data_stream, **kwargs)
+
+    def get_data(self, request=None):
+        data = next(self.child_epoch_iterator)
+        transformed_data = []
+        B, T, X, Y, C = data[0].shape
+        transformed_data.append(
+            numpy.swapaxes(
+                data[0].reshape(
+                    (B, T, X * Y * C)),
+                0, 1))
+        # Now the data shape should be T x B x F
+        transformed_data.append(data[1])
+        return transformed_data
+
+
+class ClutteredMNISTVideo(H5PYDataset):
+    def __init__(self, which_sets, **kwargs):
+        kwargs.setdefault('load_in_memory', False)
+        super(ClutteredMNISTVideo, self).__init__(
+            "/data/lisatmp3/cooijmat/datasets/cluttered-mnist-video/cluttered-mnist-video.hdf5",
+            which_sets, **kwargs)
+
+
+def get_mnist_video_streams(batch_size):
+    train_dataset = ClutteredMNISTVideo(which_sets=["train"])
+    valid_dataset = ClutteredMNISTVideo(which_sets=["valid"])
+    train_ind = numpy.arange(train_dataset.num_examples)
+    valid_ind = numpy.arange(valid_dataset.num_examples)
+    rng = numpy.random.RandomState(seed=1)
+    rng.shuffle(train_ind)
+    rng.shuffle(valid_ind)
+
+    train_datastream = DataStream.default_stream(
+        train_dataset,
+        iteration_scheme=ShuffledScheme(train_ind, batch_size))
+    train_datastream = PreprocessTransformer(train_datastream)
+
+    valid_datastream = DataStream.default_stream(
+        valid_dataset,
+        iteration_scheme=ShuffledScheme(valid_ind, batch_size))
+    valid_datastream = PreprocessTransformer(valid_datastream)
+
+    return train_datastream, valid_datastream
