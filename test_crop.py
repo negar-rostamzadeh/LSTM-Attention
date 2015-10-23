45c8fd210334bd0fdc355de77dd901359f7aae4e
Mohammad Pezeshki <mohammadpz@gmail.com>
2015-10-23 12:32:01 -0400

    testing crop

--- test_crop.py
+++ test_crop.py
@@ -32,11 +32,14 @@ patch = cropper.apply(
 f = theano.function([x, location, scale], patch, allow_input_downcast=True)
 
 tds, vds = get_mnist_video_streams(1)
-image = tds.get_epoch_iterator().next()[0][10, 0, :]
+image = tds.get_epoch_iterator().next()[0][2, 0, :]
 
-patch1 = f(image, [[60.0, 65.0]], [[1.0, 1.0]])[0, 0]
-patch2 = f(image, [[60.0, 65.0]], [[2.0, 2.0]])[0, 0]
-patch3 = f(image, [[50.0, 50.0]], [[0.28, 0.28]])[0, 0]
+import ipdb; ipdb.set_trace()
+
+location = [[60.0, 65.0]]
+scale = [[1.0, 1.0]]
+
+patch1 = f(image, location, scale)[0, 0]
 
 plt.imshow(
     image.reshape(image_shape),
@@ -49,15 +52,3 @@ plt.imshow(
     cmap=plt.gray(),
     interpolation='nearest')
 plt.savefig('img1.png')
-
-plt.imshow(
-    patch2.reshape(patch_shape),
-    cmap=plt.gray(),
-    interpolation='nearest')
-plt.savefig('img2.png')
-
-plt.imshow(
-    patch3.reshape(patch_shape),
-    cmap=plt.gray(),
-    interpolation='nearest')
-plt.savefig('img3.png')
